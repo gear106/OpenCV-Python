c547d405fed8c918e978c7cb3de61efb6bc1d980
libinglin <binglinli106@126.com>
2018-09-05 22:15:51 +0800

    modified image_gradient

--- ch18-图像梯度/image_gradient.py
+++ ch18-图像梯度/image_gradient.py
@@ -27,4 +27,15 @@ sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
 plt.subplot(2,2,1),plt.imshow(image, cmap='gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
+plt.subplot(2,2,2), plt.imshow(laplacian,cmap='gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(2,2,3), plt.imshow(sobelx, cmap='gray')
+plt.title('sobelx'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(2,2,4), plt.imshow(sobely, cmap='gray')
+plt.title('sobely'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
 
